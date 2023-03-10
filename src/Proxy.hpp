a7241e36efbbc54b8f6a174488528a5907e2d7f7
emoose <abc@cock.li>
2023-03-10 21:03:45 +0000

    Revert "switch to wrapping `nvngx.dll` only"
    This reverts commit da55c2309a511087b47f7ff01001fea8519b123c.

--- src/Proxy.hpp
+++ src/Proxy.hpp
@@ -1,26 +1,13 @@
 #pragma once
 
-// Pointers to functions inside original nvngx.dll
-extern FARPROC NVSDK_NGX_D3D11_Init_Orig;
-extern FARPROC NVSDK_NGX_D3D11_Init_Ext_Orig;
-extern FARPROC NVSDK_NGX_D3D11_Init_ProjectID_Orig;
-extern FARPROC NVSDK_NGX_D3D12_Init_Orig;
-extern FARPROC NVSDK_NGX_D3D12_Init_Ext_Orig;
-extern FARPROC NVSDK_NGX_D3D12_Init_ProjectID_Orig;
-extern FARPROC NVSDK_NGX_VULKAN_Init_Orig;
-extern FARPROC NVSDK_NGX_VULKAN_Init_Ext_Orig;
-extern FARPROC NVSDK_NGX_VULKAN_Init_ProjectID_Orig;
-extern FARPROC NVSDK_NGX_D3D12_AllocateParameters_Orig;
-extern FARPROC NVSDK_NGX_D3D12_GetCapabilityParameters_Orig;
-extern FARPROC NVSDK_NGX_D3D12_GetParameters_Orig;
-extern FARPROC NVSDK_NGX_D3D11_AllocateParameters_Orig;
-extern FARPROC NVSDK_NGX_D3D11_GetCapabilityParameters_Orig;
-extern FARPROC NVSDK_NGX_D3D11_GetParameters_Orig;
-extern FARPROC NVSDK_NGX_VULKAN_AllocateParameters_Orig;
-extern FARPROC NVSDK_NGX_VULKAN_GetCapabilityParameters_Orig;
-extern FARPROC NVSDK_NGX_VULKAN_GetParameters_Orig;
-
 namespace proxy
+{
+extern bool is_wrapping_nvngx;
+bool on_attach(HMODULE ourModule);
+void on_detach();
+};
+
+namespace proxy_nvngx
 {
 bool on_attach(HMODULE ourModule);
 void on_detach();
