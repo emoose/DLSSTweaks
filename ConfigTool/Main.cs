ec5a322a5ef4fa6b286633b3fc79f06dd33a0c35
emoose <abc@cock.li>
2023-04-12 09:17:05 +0100

    ConfigTool: fix for FirstCharToUpper (included in 0.200.6)
    forgot to commit before releasing, this fix is included in 0.200.6 already

--- ConfigTool/Main.cs
+++ ConfigTool/Main.cs
@@ -15,7 +15,7 @@ namespace DLSSTweaks.ConfigTool
             switch (input)
             {
                 case null: throw new ArgumentNullException(nameof(input));
-                case "": throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input));
+                case "": return "";
                 default: return input[0].ToString().ToUpper() + input.Substring(1);
             }
         }
