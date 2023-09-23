859cf6470b70b0b19a998ec6fb1650428858ef4d
emoose <abc@cock.li>
2023-09-23 05:37:00 +0100

    ProxyNvngx: add missing _nvngx.dll exports to fix nvngx.dll wrapper
    seems Streamline requires these for DLSSD to be supported, for some reason
    the driver nvngx.dll doesn't include them while _nvngx.dll does.
    
    unclear how SL is importing from _nvngx & nvngx simultaneously, but adding
    them and importing from the driver _nvngx instead seems to work fine

--- src/ProxyNvngx.cpp
+++ src/ProxyNvngx.cpp
@@ -27,6 +27,7 @@ FARPROC NVSDK_NGX_D3D11_CreateFeature_Orig = nullptr;
 FARPROC NVSDK_NGX_D3D11_DestroyParameters_Orig = nullptr;
 FARPROC NVSDK_NGX_D3D11_EvaluateFeature_Orig = nullptr;
 FARPROC NVSDK_NGX_D3D11_GetCapabilityParameters_Orig = nullptr;
+FARPROC NVSDK_NGX_D3D11_GetFeatureRequirements_Orig = nullptr;
 FARPROC NVSDK_NGX_D3D11_GetParameters_Orig = nullptr;
 FARPROC NVSDK_NGX_D3D11_GetScratchBufferSize_Orig = nullptr;
 FARPROC NVSDK_NGX_D3D11_Init_Orig = nullptr;
@@ -40,6 +41,7 @@ FARPROC NVSDK_NGX_D3D12_CreateFeature_Orig = nullptr;
 FARPROC NVSDK_NGX_D3D12_DestroyParameters_Orig = nullptr;
 FARPROC NVSDK_NGX_D3D12_EvaluateFeature_Orig = nullptr;
 FARPROC NVSDK_NGX_D3D12_GetCapabilityParameters_Orig = nullptr;
+FARPROC NVSDK_NGX_D3D12_GetFeatureRequirements_Orig = nullptr;
 FARPROC NVSDK_NGX_D3D12_GetParameters_Orig = nullptr;
 FARPROC NVSDK_NGX_D3D12_GetScratchBufferSize_Orig = nullptr;
 FARPROC NVSDK_NGX_D3D12_Init_Orig = nullptr;
@@ -54,17 +56,23 @@ FARPROC NVSDK_NGX_OTA_UPDATES_Install_Orig = nullptr;
 FARPROC NVSDK_NGX_OTA_UPDATES_Register_Orig = nullptr;
 FARPROC NVSDK_NGX_OTA_UPDATES_Unregister_Orig = nullptr;
 FARPROC NVSDK_NGX_OTA_UPDATES_Update_Orig = nullptr;
+FARPROC NVSDK_NGX_UpdateFeature_Orig = nullptr;
 FARPROC NVSDK_NGX_VULKAN_AllocateParameters_Orig = nullptr;
 FARPROC NVSDK_NGX_VULKAN_CreateFeature_Orig = nullptr;
 FARPROC NVSDK_NGX_VULKAN_CreateFeature1_Orig = nullptr;
 FARPROC NVSDK_NGX_VULKAN_DestroyParameters_Orig = nullptr;
 FARPROC NVSDK_NGX_VULKAN_EvaluateFeature_Orig = nullptr;
 FARPROC NVSDK_NGX_VULKAN_GetCapabilityParameters_Orig = nullptr;
+FARPROC NVSDK_NGX_VULKAN_GetFeatureDeviceExtensionRequirements_Orig = nullptr;
+FARPROC NVSDK_NGX_VULKAN_GetFeatureInstanceExtensionRequirements_Orig = nullptr;
+FARPROC NVSDK_NGX_VULKAN_GetFeatureRequirements_Orig = nullptr;
 FARPROC NVSDK_NGX_VULKAN_GetParameters_Orig = nullptr;
 FARPROC NVSDK_NGX_VULKAN_GetScratchBufferSize_Orig = nullptr;
 FARPROC NVSDK_NGX_VULKAN_Init_Orig = nullptr;
 FARPROC NVSDK_NGX_VULKAN_Init_Ext_Orig = nullptr;
+FARPROC NVSDK_NGX_VULKAN_Init_Ext2_Orig = nullptr;
 FARPROC NVSDK_NGX_VULKAN_Init_ProjectID_Orig = nullptr;
+FARPROC NVSDK_NGX_VULKAN_Init_ProjectID_Ext_Orig = nullptr;
 FARPROC NVSDK_NGX_VULKAN_ReleaseFeature_Orig = nullptr;
 FARPROC NVSDK_NGX_VULKAN_RequiredExtensions_Orig = nullptr;
 FARPROC NVSDK_NGX_VULKAN_Shutdown_Orig = nullptr;
@@ -140,6 +148,11 @@ PLUGIN_API void NVSDK_NGX_D3D11_DestroyParameters()
     NVSDK_NGX_D3D11_DestroyParameters_Orig();
 }
 
+PLUGIN_API void NVSDK_NGX_D3D11_GetFeatureRequirements()
+{
+    NVSDK_NGX_D3D11_GetFeatureRequirements_Orig();
+}
+
 PLUGIN_API void NVSDK_NGX_D3D11_GetScratchBufferSize()
 {
     NVSDK_NGX_D3D11_GetScratchBufferSize_Orig();
@@ -168,6 +181,11 @@ PLUGIN_API void NVSDK_NGX_D3D12_CreateFeature()
 PLUGIN_API void NVSDK_NGX_D3D12_DestroyParameters()
 {
     NVSDK_NGX_D3D12_DestroyParameters_Orig();
+}
+
+PLUGIN_API void NVSDK_NGX_D3D12_GetFeatureRequirements()
+{
+    NVSDK_NGX_D3D12_GetFeatureRequirements_Orig();
 }
 
 PLUGIN_API void NVSDK_NGX_D3D12_GetScratchBufferSize()
@@ -220,6 +238,11 @@ PLUGIN_API void NVSDK_NGX_OTA_UPDATES_Update()
     NVSDK_NGX_OTA_UPDATES_Update_Orig();
 }
 
+PLUGIN_API void NVSDK_NGX_UpdateFeature()
+{
+    NVSDK_NGX_UpdateFeature_Orig();
+}
+
 PLUGIN_API void NVSDK_NGX_VULKAN_CreateFeature()
 {
     NVSDK_NGX_VULKAN_CreateFeature_Orig();
@@ -233,6 +256,21 @@ PLUGIN_API void NVSDK_NGX_VULKAN_CreateFeature1()
 PLUGIN_API void NVSDK_NGX_VULKAN_DestroyParameters()
 {
     NVSDK_NGX_VULKAN_DestroyParameters_Orig();
+}
+
+PLUGIN_API void NVSDK_NGX_VULKAN_GetFeatureDeviceExtensionRequirements()
+{
+    NVSDK_NGX_VULKAN_GetFeatureDeviceExtensionRequirements_Orig();
+}
+
+PLUGIN_API void NVSDK_NGX_VULKAN_GetFeatureInstanceExtensionRequirements()
+{
+    NVSDK_NGX_VULKAN_GetFeatureInstanceExtensionRequirements_Orig();
+}
+
+PLUGIN_API void NVSDK_NGX_VULKAN_GetFeatureRequirements()
+{
+    NVSDK_NGX_VULKAN_GetFeatureRequirements_Orig();
 }
 
 PLUGIN_API void NVSDK_NGX_VULKAN_GetScratchBufferSize()
@@ -277,13 +315,16 @@ bool on_attach(HMODULE ourModule)
     if (status != ERROR_SUCCESS)
         return false;
 
-    std::filesystem::path nvngxPath = ngxCorePath;
-    nvngxPath = nvngxPath / "nvngx.dll";
-    std::wstring nvngxPathStr = nvngxPath.wstring();
-
-    proxy::origModule = LoadLibraryW(nvngxPathStr.c_str());
+    
+    auto nvngxModulePath = (std::filesystem::path(ngxCorePath) / "_nvngx.dll").wstring();
+    proxy::origModule = LoadLibraryW(nvngxModulePath.c_str());
     if (!proxy::origModule)
-        return false;
+    {
+        nvngxModulePath = (std::filesystem::path(ngxCorePath) / "nvngx.dll").wstring();
+        proxy::origModule = LoadLibraryW(nvngxModulePath.c_str());
+        if (!proxy::origModule)
+            return false;
+    }
 
     NVSDK_NGX_CUDA_AllocateParameters_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_CUDA_AllocateParameters");
     NVSDK_NGX_CUDA_CreateFeature_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_CUDA_CreateFeature");
@@ -302,6 +343,7 @@ bool on_attach(HMODULE ourModule)
     NVSDK_NGX_D3D11_DestroyParameters_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_D3D11_DestroyParameters");
     NVSDK_NGX_D3D11_EvaluateFeature_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_D3D11_EvaluateFeature");
     NVSDK_NGX_D3D11_GetCapabilityParameters_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_D3D11_GetCapabilityParameters");
+    NVSDK_NGX_D3D11_GetFeatureRequirements_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_D3D11_GetFeatureRequirements");
     NVSDK_NGX_D3D11_GetParameters_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_D3D11_GetParameters");
     NVSDK_NGX_D3D11_GetScratchBufferSize_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_D3D11_GetScratchBufferSize");
     NVSDK_NGX_D3D11_Init_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_D3D11_Init");
@@ -315,6 +357,7 @@ bool on_attach(HMODULE ourModule)
     NVSDK_NGX_D3D12_DestroyParameters_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_D3D12_DestroyParameters");
     NVSDK_NGX_D3D12_EvaluateFeature_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_D3D12_EvaluateFeature");
     NVSDK_NGX_D3D12_GetCapabilityParameters_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_D3D12_GetCapabilityParameters");
+    NVSDK_NGX_D3D12_GetFeatureRequirements_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_D3D12_GetFeatureRequirements");
     NVSDK_NGX_D3D12_GetParameters_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_D3D12_GetParameters");
     NVSDK_NGX_D3D12_GetScratchBufferSize_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_D3D12_GetScratchBufferSize");
     NVSDK_NGX_D3D12_Init_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_D3D12_Init");
@@ -329,17 +372,23 @@ bool on_attach(HMODULE ourModule)
     NVSDK_NGX_OTA_UPDATES_Register_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_OTA_UPDATES_Register");
     NVSDK_NGX_OTA_UPDATES_Unregister_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_OTA_UPDATES_Unregister");
     NVSDK_NGX_OTA_UPDATES_Update_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_OTA_UPDATES_Update");
+    NVSDK_NGX_UpdateFeature_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_UpdateFeature");
     NVSDK_NGX_VULKAN_AllocateParameters_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_AllocateParameters");
     NVSDK_NGX_VULKAN_CreateFeature_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_CreateFeature");
     NVSDK_NGX_VULKAN_CreateFeature1_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_CreateFeature1");
     NVSDK_NGX_VULKAN_DestroyParameters_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_DestroyParameters");
     NVSDK_NGX_VULKAN_EvaluateFeature_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_EvaluateFeature");
     NVSDK_NGX_VULKAN_GetCapabilityParameters_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_GetCapabilityParameters");
+    NVSDK_NGX_VULKAN_GetFeatureDeviceExtensionRequirements_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_GetFeatureDeviceExtensionRequirements");
+    NVSDK_NGX_VULKAN_GetFeatureInstanceExtensionRequirements_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_GetFeatureInstanceExtensionRequirements");
+    NVSDK_NGX_VULKAN_GetFeatureRequirements_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_GetFeatureRequirements");
     NVSDK_NGX_VULKAN_GetParameters_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_GetParameters");
     NVSDK_NGX_VULKAN_GetScratchBufferSize_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_GetScratchBufferSize");
     NVSDK_NGX_VULKAN_Init_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_Init");
     NVSDK_NGX_VULKAN_Init_Ext_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_Init_Ext");
+    NVSDK_NGX_VULKAN_Init_Ext2_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_Init_Ext2");
     NVSDK_NGX_VULKAN_Init_ProjectID_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_Init_ProjectID");
+    NVSDK_NGX_VULKAN_Init_ProjectID_Ext_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_Init_ProjectID_Ext");
     NVSDK_NGX_VULKAN_ReleaseFeature_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_ReleaseFeature");
     NVSDK_NGX_VULKAN_RequiredExtensions_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_RequiredExtensions");
     NVSDK_NGX_VULKAN_Shutdown_Orig = GetProcAddress(proxy::origModule, "NVSDK_NGX_VULKAN_Shutdown");
